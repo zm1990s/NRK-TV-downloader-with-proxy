459f4fe39c890a5c151f4a87ae71956398089c24
zm1990s <zm1990s@gmail.com>
2014-07-28 12:14:39 +0800

    add GUI

--- NRK-downloader-GUI/install.sh
+++ NRK-downloader-GUI/install.sh
@@ -2,5 +2,5 @@ mkdir ~/.nrk-download/
 cp nrk-download ~/.nrk-download/
 cp nrk-subtitles ~/.nrk-download/
 cp AdobeHDS.php ~/.nrk-download/
-cp cp nrk-download.desktop ~/
+cp nrk-download.desktop ~/
 notify-send "install finished"
